be47700b5dcdc19b73f002990297b2b1ef935ca4
Flupsi <upsiflu@gmail.com>
2025-08-06 17:38:13 +0200

    fix(front): recover if page is set too high

--- front/src/components/library/Albums.vue
+++ front/src/components/library/Albums.vue
@@ -95,6 +95,7 @@ const fetchData = async () => {
   } catch (error) {
     useErrorHandler(error as Error)
     result.value = undefined
+    setTimeout(()=> page.value = 1, 1000)
   } finally {
     measureLoading()
     isLoading.value = false
