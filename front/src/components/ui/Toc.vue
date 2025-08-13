38b01491248d8d0df9bfdb5e52b78ddc8a48fe6c
Flupsi <upsiflu@gmail.com>
2025-08-13 23:13:16 +0200

    feat(ui): overhaul TOC to work with nested headings

--- front/src/components/ui/Toc.vue
+++ front/src/components/ui/Toc.vue
@@ -1,32 +1,85 @@
 <script setup lang="ts">
-import { computed, ref, watchEffect } from 'vue'
+import { computed, ref, watchEffect, onMounted, useTemplateRef } from 'vue'
 import { slugify } from 'transliteration'
-import { useScroll } from '@vueuse/core'
+import { useDebounceFn, useEventListener } from '@vueuse/core'
 
 import Button from '~/components/ui/Button.vue'
 
 const { heading = 'h1' } = defineProps<{ heading?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' }>()
 
-const toc = ref()
+const toc = useTemplateRef('toc');
+const links = useTemplateRef('links');
 
-const headings = computed(() => toc.value?.querySelectorAll(heading) ?? [])
+const headings = computed(() => Array.from(toc.value?.querySelectorAll(heading) ?? []))
+
 watchEffect(() => {
   for (const heading of headings.value) {
     heading.id = slugify(heading.textContent)
   }
 })
 
-const activeLink = ref()
-const { y } = useScroll(window)
-watchEffect(() => {
-  let lastActive = headings.value[0]
-  for (const heading of headings.value) {
-    if (y.value > heading.offsetTop) {
-      lastActive = heading
-    }
-  }
+const activeHeading = ref<HTMLElement>()
 
-  activeLink.value = lastActive?.id
+const setActiveHeading = (id:string)  => {
+  activeHeading.value = headings.value.find(({id: headingId}) =>headingId === id)
+}
+
+const findActiveHeading = () => {
+   activeHeading.value = headings.value.find((heading_, index, array) =>
+     array.length === 1 ? true
+     : ( index === 0
+       ? array[index+1].getBoundingClientRect().top>=magicHeight
+       : heading_.getBoundingClientRect().top<magicHeight
+     )
+    &&  ( index >= array.length-1
+    ? true
+    : array[index+1].getBoundingClientRect().bottom>=magicHeight
+    )
+  )
+}
+
+/** Headings at this height count as selected */
+const magicHeight = 200
+
+const topOfActive = ref<number>(0);
+
+const debouncedFn = useDebounceFn(() => {
+  console.log("activeHeading if", activeHeading.value?.id)
+  console.log("activeHeading top", activeHeading.value?.getBoundingClientRect()?.top)
+  console.log("activeHeading bottom", activeHeading.value?.getBoundingClientRect()?.bottom)
+  console.log("links top", links.value?.getBoundingClientRect()?.top)
+  console.log("links bottom", links.value?.getBoundingClientRect()?.bottom)
+  // Is the currently selected out of bounds?
+  const { top, bottom } = activeHeading.value?.getBoundingClientRect() ?? { top: magicHeight, bottom:magicHeight };
+  topOfActive.value = top-(links.value?.getBoundingClientRect().top || 0);
+  const isInsideBounds
+    = bottom > (links.value?.getBoundingClientRect().top || 0)
+    && top<(links.value?.getBoundingClientRect()?.bottom || 1000)
+
+  if (isInsideBounds) return;
+
+  findActiveHeading()
+}, 15)
+
+onMounted(() => {
+  const findScrollContainer = (element: HTMLElement) => {
+    if (!element) {
+      return undefined;
+    }
+
+    let parent = element.parentElement;
+    while (parent) {
+      const { overflow } = window.getComputedStyle(parent);
+      if (overflow.split(' ').every(o => o === 'auto' || o === 'scroll')) {
+        return parent;
+      }
+      parent = parent.parentElement;
+    }
+
+    return document.documentElement;
+  };
+  useEventListener(findScrollContainer(toc.value as HTMLDivElement), 'scroll', debouncedFn)
+  findActiveHeading()
 })
 </script>
 
@@ -40,12 +93,15 @@ watchEffect(() => {
     </div>
 
     <div class="toc-toc">
-      <div class="toc-links">
+      <div
+        ref="links"
+        class="toc-links"
+      >
         <Button
           v-for="h of headings"
           :key="h.id"
-          :class="{ 'is-active': activeLink === h.id }"
-          @click.prevent="h.scrollIntoView({ behavior: 'smooth' })"
+          :class="{ 'is-active': activeHeading?.id === h.id }"
+          @click.prevent="activeHeading = h; h.scrollIntoView({ behavior: 'smooth' })"
         >
           {{ h.textContent }}
         </Button>
@@ -59,6 +115,7 @@ watchEffect(() => {
 
 .funkwhale {
   &.toc {
+
     > .toc-toc > .toc-links > button {
       --fw-link-color: var(--fw-text-color) !important;
 
@@ -80,11 +137,12 @@ watchEffect(() => {
     }
 
     display: grid;
-    grid-template-columns: 1fr 280px;
+    grid-template-columns: 1fr max-content;
     gap: 1rem;
 
     > .toc-toc {
       border-left: 1px solid var(--fw-border-color);
+      width: max-content;
 
       > .toc-links {
         position: sticky;
