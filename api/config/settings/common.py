4bac85e4c0bdbf0b7c03d29b49d49f3d45376651
Petitminion <petitminion@riseup.net>
2025-09-02 12:22:59 +0200

    fix(back):API_AUTHENTICATION_REQUIRED default to true

--- api/config/settings/common.py
+++ api/config/settings/common.py
@@ -1237,7 +1237,7 @@ Example:
 
 - ``signup=5/d,password-reset=2/d,anonymous-reports=5/d``
 """
-API_AUTHENTICATION_REQUIRED = env.bool("API_AUTHENTICATION_REQUIRED", default=False)
+API_AUTHENTICATION_REQUIRED = env.bool("API_AUTHENTICATION_REQUIRED", default=True)
 """used by APIAutenticationRequired to allow or not access without authentication"""
 
 BROWSABLE_API_ENABLED = env.bool("BROWSABLE_API_ENABLED", default=False)
