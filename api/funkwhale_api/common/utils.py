6e5c2bcaf327c49525b083c0e0bf1c9c2a7f0b61
PetitMinion <petitminion@riseup.net>
2026-03-03 12:08:37 +0100

    fix(back):ChunkedPath did not sanitize filename

--- api/funkwhale_api/common/utils.py
+++ api/funkwhale_api/common/utils.py
@@ -80,7 +80,7 @@ class ChunkedPath:
         self.preserve_file_name = preserve_file_name
 
     def __call__(self, instance, filename):
-        self.sanitize_filename(filename)
+        filename = self.sanitize_filename(filename)
         uid = str(uuid.uuid4())
         chunk_size = 2
         chunks = [uid[i : i + chunk_size] for i in range(0, len(uid), chunk_size)]
